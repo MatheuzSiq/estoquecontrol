abf76ce3549157a8d2999888859c5a646544a3a2
MatheuzSiq <matheus.siqueira@a.fecaf.com.br>
2026-03-30 22:56:57 -0300

    feat: configuração de tipos do NextAuth e rotas de estoque

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const globalForPrisma = global as any;
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
